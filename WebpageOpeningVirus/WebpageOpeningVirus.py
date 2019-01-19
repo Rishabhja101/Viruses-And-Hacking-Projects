9484c6bdec603bebbbb3e6a6a601f6bee3a6bf20
Rishabh Jain <rishabh101.99@gmail.com>
2019-01-18 22:19:16 -0800

    setup script to hide command prompt window

--- WebpageOpeningVirus/WebpageOpeningVirus.py
+++ WebpageOpeningVirus/WebpageOpeningVirus.py
@@ -8,6 +8,7 @@ ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 6 )
 while True:
     sites = random.choice(['google.com', 'bing.com', 'yahoo.com'])
     visit = "http://{}".format(sites)
+    ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 6 )
     webbrowser.open(visit)
     delay = random.randrange(10,20)
     time.sleep(delay)
