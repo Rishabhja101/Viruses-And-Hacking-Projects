7886444a6e55f1cc70c3d5d3d77ee9057198f868
Rishabh Jain <rishabh101.99@gmail.com>
2019-01-27 14:12:58 -0800

    clears previous files within the repository

--- GithubCommitSpammer/GithubCommitSpammer.py
+++ GithubCommitSpammer/GithubCommitSpammer.py
@@ -9,7 +9,13 @@ def setDate(date):
     os.system("date " + (str(date.month) + "-" + str(date.day)  + "-" + str(date.year)))
     print((str(date.month) + "-" + str(date.day)  + "-" + str(date.year)))
 
+def deleteFiles(directory):
+    filelist = [ f for f in os.listdir(directory) if f.endswith(".txt") ]
+    for f in filelist:
+        os.remove(os.path.join(directory, f))
+
 def spamCommits(date):
+    deleteFiles("C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam")
     for i in range(1, 366):
         setDate(date)
         createCommit(str(i), "created new file")
