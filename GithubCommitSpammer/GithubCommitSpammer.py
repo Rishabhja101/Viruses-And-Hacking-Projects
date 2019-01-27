a61d4e2885a47283c588ee71b1bf342497336d5c
Rishabh Jain <rishabh101.99@gmail.com>
2019-01-26 18:12:50 -0800

    able to change name of file and commit message

--- GithubCommitSpammer/GithubCommitSpammer.py
+++ GithubCommitSpammer/GithubCommitSpammer.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 
-def createCommit(filename):
-    os.system('cd C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam & git init & echo test > C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam\\' + filename + '.txt & git add . & git commit -m \"test commit\" & git remote -v & git push origin master')
+def createCommit(filename, commitmessage):
+    os.system('cd C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam & git init & echo test > C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam\\' + filename + '.txt & git add . & git commit -m \"' + commitmessage + '\" & git remote -v & git push origin master')
 
 githubURL = "https://github.com/Rishabhja101/Spam.git"
 localURL = "C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam"
 
-createCommit('0')
+createCommit('0', "test commit")
