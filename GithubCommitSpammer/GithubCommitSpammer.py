8923e6cbe4448a1db90590cd8fc648ffa3e8cb10
Rishabh Jain <rishabh101.99@gmail.com>
2019-01-27 13:37:01 -0800

    able to change system date when run as administrator

--- GithubCommitSpammer/GithubCommitSpammer.py
+++ GithubCommitSpammer/GithubCommitSpammer.py
@@ -1,28 +1,17 @@
 import os
 import sys
 import datetime
-import pywin32
 
 def createCommit(filename, commitmessage):
     os.system('cd C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam & git init & echo test > C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam\\' + filename + '.txt & git add . & git commit -m \"' + commitmessage + '\" & git remote -v & git push origin master')
 
-def _win_set_time(time):
-    dayOfWeek = datetime.datetime(time).isocalendar()[2]
-    pywin32.SetSystemTime( time[:2] + (dayOfWeek,) + time[2:])
 
 githubURL = "https://github.com/Rishabhja101/Spam.git"
 localURL = "C:\\Users\\risha\\OneDrive\\Documents\\GitHub\\Spam"
 
-startTime = ( 2018, # Year
-              1, # Month
-              1, # Day
-              0, # Hour
-              0, # Minute
-              0, # Second
-              0, # Millisecond
-          )
 
-_win_set_time(startTime)
+date = datetime.date(1943,3, 31)
+date += datetime.timedelta(days=1)
+print (str(date.month) + "-" + str(date.day)  + "-" + str(date.year))
 
-#for i in range(1, 365):
-#    createCommit(i, "test commit")
+os.system("date 04/01/18")
