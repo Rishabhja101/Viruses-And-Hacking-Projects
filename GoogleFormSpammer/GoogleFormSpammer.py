e4bda87d9de909656c338a4cc8c79223420772bd
Rishabh Jain <rishabh101.99@gmail.com>
2019-02-28 23:05:26 -0800

    project complete

--- GoogleFormSpammer/GoogleFormSpammer.py
+++ GoogleFormSpammer/GoogleFormSpammer.py
@@ -3,17 +3,24 @@ import time
 
 url = 'https://docs.google.com/forms/d/1LaZf1hIPnfl8MCbv6M5oJ0P0DK-ice4qOe3kNACrPT4/viewform?vc=0&c=0&w=1&edit_requested=true'
 xpathCheckbox = '//*[@id="mG61Hd"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/label/div/div[1]'
-#xpathSubmitButton = '//*[@id="mG61Hd"]/div/div[2]/div[3]/div/div/div/content/span'
 xpathSubmitButton = '//*[@id="mG61Hd"]/div/div[2]/div[3]/div/div/div'
-
+xpathSubmitAnotherResponce = '/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/a'
 
 driver = webdriver.Edge()
 driver.get(url)
 
-checkbox = driver.find_element_by_xpath(xpathCheckbox)
-checkbox.click()
+for i in range (0, 10):
+    checkbox = driver.find_element_by_xpath(xpathCheckbox)
+    checkbox.click()
 
-time.sleep(0.5)
+    #time.sleep(0.2)
 
-button = driver.find_element_by_xpath(xpathSubmitButton)
-button.click()
+    button = driver.find_element_by_xpath(xpathSubmitButton)
+    button.click()
+
+    time.sleep(0.5)
+
+    repeat = driver.find_element_by_xpath(xpathSubmitAnotherResponce)
+    repeat.click()
+
+    time.sleep(0.5)
